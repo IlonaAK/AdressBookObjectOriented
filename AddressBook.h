7d97dd37b7e4b21b719993fccf8873efdc51df1f
Ilona Lelek <lelek.ilona.programista@gmail.com>
2020-08-16 15:05:55 +0200

    Create base class AddressBook and User

---
+++ AddressBook.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "User.h"
+
+using namespace std;
+
+class AddressBook
+{
+    int idOfLoggedUser;
+    int idLastAddresse;
+    int idDeletedAddressee;
+
+    vector <User> users;
+
+public:
+    registerationOfUser();
+};
+
+
---
+++ User.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class User
+{
+    int dd;
+    string name, password;
+};
